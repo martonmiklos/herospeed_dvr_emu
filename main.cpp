caaf148e77ec28cf7e8db4ea8a40328c1b6e2d29
Miklos Marton <martonmiklosqdev@gmail.com>
2015-08-27 08:54:50 +0200

    Filled app info in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setOrganizationName("%ORGANIZATION%");
-    QCoreApplication::setApplicationName("%APPNAME%");
-    QCoreApplication::setApplicationVersion("%APPVERSION%");
+    QCoreApplication::setOrganizationName("MM");
+    QCoreApplication::setApplicationName("Herospeed DVR emulator");
+    QCoreApplication::setApplicationVersion("0.1");
 
     QApplication a(argc, argv);
     MainWindow w;
